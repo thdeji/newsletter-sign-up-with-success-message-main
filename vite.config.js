4a59da584a1e7bc92b340ba87e4ef56c6c8397de
thdeji <omotobaadedeji@gmail.com>
2025-12-05 02:22:12 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/newsletter-sign-up-with-success-message-main/",
   plugins: [
     react({
       babel: {
